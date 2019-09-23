3b59a63fb96925da9ea493ce9b82d926b3f5a2bd
minhdien1710 <nguyenminhdien996@gmail.com>
2019-09-23 17:00:51 +0700

    added

--- src/controllers/Converter.java
+++ src/controllers/Converter.java
@@ -2,17 +2,21 @@ package controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class Converter {
-    @GetMapping("convert")
-    public String convert(Model model){
-        float usd= 3;
-        int rate = 23000;
-        float vnd = rate * usd;
+    @GetMapping("/")
+    public String convertForm(){
+        return "index";
+    }
+    @PostMapping("/convert")
+    public String convert(@RequestParam double usd,@RequestParam double rate, Model model){
         model.addAttribute("usd", usd);
         model.addAttribute("rate", rate);
+        double vnd = usd*rate;
         model.addAttribute("vnd", vnd);
         return "index";
-
     }
 }
